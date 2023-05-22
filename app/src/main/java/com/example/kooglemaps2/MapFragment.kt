aceb37f05d1ca016fe2e7d498a175e91166ab991
Glyph <newdy0808@gmail.com>
2023-05-22 22:42:22 +0900

    test

--- app/src/main/java/com/example/kooglemaps2/MapFragment.kt
+++ app/src/main/java/com/example/kooglemaps2/MapFragment.kt
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// the fragment initialization parameters, e.g. A RG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
