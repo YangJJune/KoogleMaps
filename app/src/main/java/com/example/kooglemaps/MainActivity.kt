a1f62da38e4ea2cceea12f4aaae56c83da99132c
YangJJune <ysj7191@naver.com>
2023-05-29 19:33:00 +0900

    20230529

--- app/src/main/java/com/example/kooglemaps/MainActivity.kt
+++ app/src/main/java/com/example/kooglemaps/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kooglemaps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.kooglemaps.databinding.ActivityMainBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -18,13 +21,16 @@ class MainActivity : AppCompatActivity() {
         var dbCon = dbController() //dbController
         data = dbCon.getData()
     }
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
 
+    }
     fun initLayout(){
         binding.loginBtn.setOnClickListener {
-
+            Toast.makeText(this,"로그인 시도", Toast.LENGTH_SHORT).show()
         }
         binding.registerBtn.setOnClickListener {
-
+            val i = Intent(this@MainActivity, RegisterActivity::class.java)
+            launcher.launch(i)
         }
     }
 }
---
+++ app/src/main/java/com/example/kooglemaps/RegisterActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kooglemaps
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kooglemaps.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity:AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var registerBinding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+        Toast.makeText(this,"11",Toast.LENGTH_SHORT).show()
+        registerBinding.registerBtn.setOnClickListener {
+            
+            val email:String = registerBinding.emailedit.text.toString()
+            val pw:String = registerBinding.passwordEdit.text.toString()
+            if(pw.equals(registerBinding.passwordEdit2.text.toString())) {
+                auth.createUserWithEmailAndPassword(email,pw).addOnCompleteListener(this) { task->
+                    if(task.isSuccessful){
+                        Toast.makeText(this,"성공",Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                        Toast.makeText(this,"실패",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+
+}
