8f0120407f8abb889781bf0190773eb9a09261a2
Glyph <newdy0808@gmail.com>
2023-06-02 00:10:56 +0900

    지도, 플로팅 버튼 시작, 확대축소 및 지역범위 제한

--- app/src/main/java/com/example/kooglemaps2/MainActivity.kt
+++ app/src/main/java/com/example/kooglemaps2/MainActivity.kt
@@ -6,20 +6,21 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.kooglemaps2.R
 import com.example.kooglemaps2.databinding.ActivityMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-class MainActivity : AppCompatActivity(){
-    lateinit var binding :ActivityMainBinding
+class MainActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener{
+    lateinit var binding : ActivityMainBinding
     lateinit var googleMap: GoogleMap
+
+    private var clickedMarker: Marker ?= null
 
     val loc = LatLng(37.554752, 126.970631)//서울역 좌표
     val konkuk_loc = LatLng(37.542402, 127.076903)
@@ -31,11 +32,22 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         initmap()
 
-        initspinner()
+        //initspinner()
+
+        initLayout()
     }
 
+    private fun initLayout() {
+        binding.btnFab.setOnClickListener {
+
+        }
+    }
+
+
+    /*
     private fun initspinner() {
         //스피너 초기화
         //스피너는 어댑터가 필요한 유닛이므로 어댑터 먼저 만든다.
@@ -52,7 +64,7 @@ class MainActivity : AppCompatActivity(){
             spinner.adapter = adapter//어댑터 부착
             
             spinner.setSelection(1) //map type의 디폴트를 normal로 설정
-            
+
             //스피너 이벤트 처리 -> 맵 타입 바꾸도록
             spinner.onItemSelectedListener = object : OnItemSelectedListener{
                 override fun onItemSelected(
@@ -86,6 +98,9 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+
+
+     */
     private fun initmap() {
         //초기화 작업 -> 구글맵 초기화는 map fragment를 통해서 가능하므로 접근
         val mapFragment = supportFragmentManager
@@ -102,18 +117,87 @@ class MainActivity : AppCompatActivity(){
             googleMap.setMinZoomPreference(16.0f)//zoom의 최소 레벨 설정
             googleMap.setMaxZoomPreference(22.0f)//zoom 최대 레벨 설정
 
+            //val konkuk_loc = LatLng(37.542402, 127.076903)
+            val bounds = LatLngBounds.Builder()
+                .include(LatLng(37.547660, 127.074053)) // 좌상단 좌표
+                .include(LatLng(37.538032, 127.081679)) // 우하단 좌표
+                .build()
+
+
+            googleMap.setLatLngBoundsForCameraTarget(bounds)
+            /*
+            //사용자가 학교 근처를 넘어서려고 할 때, 다시 돌아오도록 하는 코드
+            googleMap.setOnCameraMoveListener {
+                val cameraBounds = googleMap.projection.visibleRegion.latLngBounds
+                if (!bounds.contains(cameraBounds.center)) {
+                    val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 0)
+                    googleMap.moveCamera(cameraUpdate)
+                }
+            }
+            */
+
+
             //맵 마커
             val option = MarkerOptions()
             option.position(konkuk_loc)
             option.icon(
                 BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
             )
-            option.title("역")//마커의 윗쪽 큰글씨
-            option.snippet("서울역")//마커의 아랫쪽 작은글씨
+            option.title("청심대")//마커의 윗쪽 큰글씨
+            option.snippet("그늘 굿")//마커의 아랫쪽 작은글씨
             googleMap.addMarker(option)?.showInfoWindow()
+
+            googleMap.setOnMapClickListener { latLng ->
+                // 클릭한 위치에 마커를 추가합니다.
+                googleMap.addMarker(MarkerOptions().position(latLng).title("Clicked Marker"))
+            }
+
+            googleMap.setOnMarkerClickListener(this)
+            googleMap.setOnMapClickListener(this)
+
         }
 
     }
 
+    override fun onMarkerClick(marker: Marker): Boolean {
+        if (marker.title == "Clicked Marker") {
+            showInputDialog(marker)
+        }
+        return false
+    }
+    private fun showInputDialog(marker: Marker) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("마커 제목 입력")
+
+        val input = EditText(this)
+        builder.setView(input)
+
+        builder.setPositiveButton("확인") { _, _ ->
+            val title = input.text.toString().trim()
+            if (title.isNotEmpty()) {
+                marker?.title = title
+            } else {
+                marker?.remove()
+            }
+        }
+
+        builder.setNegativeButton("취소") { dialog, _ ->
+            marker?.remove()
+            dialog.dismiss()
+        }
+
+        builder.show()
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        // 기존 마커가 있는 경우 제거합니다.
+        //clickedMarker?.remove()
+
+        // 마커를 추가합니다.
+        clickedMarker = googleMap.addMarker(MarkerOptions().position(p0).title("Click to set title"))
+
+        // 사용자로부터 제목을 입력받는 다이얼로그를 표시합니다.
+        showInputDialog(clickedMarker!!)
+    }
 
 }
