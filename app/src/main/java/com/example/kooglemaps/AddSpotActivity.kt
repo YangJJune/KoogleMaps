74c0e1b0adfe1b43ac350015777110d666e7870f
kim <kimjisu4939@gmail.com>
2023-06-09 15:30:00 +0900

    230609 googleMap과 SpotActivity 및 AddSpotActivity 연결

--- app/src/main/java/com/example/kooglemaps/AddSpotActivity.kt
+++ app/src/main/java/com/example/kooglemaps/AddSpotActivity.kt
@@ -9,10 +9,18 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kooglemaps.databinding.ActivityAddSpotBinding
 import com.example.kooglemaps.databinding.ActivitySpotBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
 // spot 추가하는 작업 수행
 class AddSpotActivity: AppCompatActivity() {
     lateinit var binding: ActivityAddSpotBinding
+    lateinit var googleMap : GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,7 @@ class AddSpotActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         initLayout()
+        initmap()
     }
 
     private fun initLayout() {
@@ -41,6 +50,7 @@ class AddSpotActivity: AppCompatActivity() {
                     Toast.makeText(this@AddSpotActivity,
                         "스팟 이름은 필수로 입력해야 합니다!", Toast.LENGTH_SHORT).show()
                 }
+
                 // 지도 위치 선택했는지 확인
                 //else if(){
                 // }
@@ -135,5 +145,65 @@ class AddSpotActivity: AppCompatActivity() {
                 }
             })
         }
+    }
+
+    private fun initmap() {
+        val loc = intent.getStringExtra("loc")
+        //lat/lng: (37.542402,127.076903) 로 나온다.
+
+        val regex = Regex("""\d+\.\d+""")
+        val matches = regex.findAll(loc!!)
+        val numbers = matches.map { it.value }.toList()
+
+        var x :Double = 0.0
+        var y :Double = 0.0
+
+        if (numbers.size == 2) {
+            x= numbers[0].toDouble()
+            y= numbers[1].toDouble()
+
+            // x와 y 변수 사용 예시
+            println("x: $x")
+            println("y: $y")
+        } else {
+            println("좌표를 찾을 수 없습니다.")
+        }
+        //여기까지 클릭한 마커의 좌표 가져오기
+
+        //초기화 작업 -> 구글맵 초기화는 map fragment를 통해서 가능하므로 접근
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map_add_spot)
+                as SupportMapFragment//map fragment로 변환
+        mapFragment.getMapAsync{
+            googleMap = it
+            //구글맵 객체 로딩 완료되면 카메라 이동!
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(x, y),18.0f))
+            //zoom level 제한하기
+            googleMap.setMinZoomPreference(18.0f)//zoom의 최소 레벨 설정
+            googleMap.setMaxZoomPreference(18.0f)//zoom 최대 레벨 설정
+
+            //val konkuk_loc = LatLng(37.542402, 127.076903)
+            val bounds = LatLngBounds.Builder()
+                .include(LatLng(x, y)) // 좌상단 좌표
+                .include(LatLng(x, y)) // 우하단 좌표
+                .build()
+
+            //맵 마커
+            val option = MarkerOptions()
+            option.position(LatLng(x, y))
+            option.icon(
+                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+            )
+            option.title("스팟 추가 위치")//마커의 윗쪽 큰글씨
+            googleMap.addMarker(option)?.showInfoWindow()
+
+            googleMap.setOnMapClickListener { latLng ->
+                // 클릭한 위치에 마커를 추가합니다.
+                googleMap.addMarker(MarkerOptions().position(latLng).title("Clicked Marker"))
+            }
+
+            googleMap.setLatLngBoundsForCameraTarget(bounds)
+        }
+
     }
 }
