b3a4dc42d1293f6316cd873376f6a2db9ded0688
Roysung0625 <roy06255@gmail.com>
2023-06-20 15:16:16 +0900

    6/20 spotActivity 제목 표시되게 수정

--- app/src/main/java/com/example/kooglemaps/SpotActivity.kt
+++ app/src/main/java/com/example/kooglemaps/SpotActivity.kt
@@ -169,7 +169,8 @@ class SpotActivity: AppCompatActivity() {
                 BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
             )
             option.title(title)//마커의 윗쪽 큰글씨
-            option.snippet(content)//마커의 아랫쪽 작은글씨
+            if(content != "")
+                option.snippet(content)//마커의 아랫쪽 작은글씨
             googleMap.addMarker(option)?.showInfoWindow()
 
             googleMap.setOnMapClickListener { latLng ->
@@ -180,6 +181,7 @@ class SpotActivity: AppCompatActivity() {
             googleMap.setLatLngBoundsForCameraTarget(bounds)
 
             binding.apply {
+                spotName.text = title
                 favoriteNum.text = like.size.toString()
                 spotDescription.text = desc
             }
